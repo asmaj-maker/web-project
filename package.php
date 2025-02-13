aff992c1554b6ad8bef79b7ee88095bb89478e7c
Asmajawfar <asmajawfar123@gmail.com>
2025-02-13 18:17:42 +0530

    git

--- package.php
+++ package.php
@@ -5,7 +5,6 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Package</title>
-
    <!-- Swiper CSS link -->
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
